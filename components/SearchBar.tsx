8e684247c1c4682fb4b2aaa99f97e638712346cf
Guillermina Lorenzo Martinez <glm1a23@soton.ac.uk>
2023-07-05 13:35:56 +0100

    Updated search bar component, added close button

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -10,9 +10,15 @@ const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
   return (
     <View style={styles.container}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.input} placeholder="Search" value="" />
-        <TouchableOpacity onPress={onSearch}>
-          <FontAwesome name="search" size={20} color="black" />
+        <TextInput style={styles.input} placeholder="Search" />
+        <TouchableOpacity
+          onPress={() => console.log("Close clicked")}
+          style={styles.closeButton}
+        >
+          <FontAwesome name="close" size={25} color="grey" />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={onSearch} style={styles.searchButton}>
+          <FontAwesome name="search" size={25} color="black" />
         </TouchableOpacity>
       </View>
     </View>
@@ -23,6 +29,7 @@ const styles = StyleSheet.create({
   container: {
     alignItems: "center",
     marginTop: 40,
+    marginHorizontal: 10,
   },
   inputContainer: {
     flexDirection: "row",
@@ -34,7 +41,13 @@ const styles = StyleSheet.create({
   },
   input: {
     flex: 1,
-    height: 40,
+    height: 50,
+  },
+  closeButton: {
+    marginRight: 30,
+  },
+  searchButton: {
+    marginRight: 10,
   },
 });
 
