a6611e506ae509e16ef8d2b8462773fa42ace9fd
Guillermina Lorenzo Martinez <glm1a23@soton.ac.uk>
2023-07-05 16:48:47 +0100

    Updated HomeScreen, deleted footer

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,17 +1,22 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, SafeAreaView } from "react-native";
 import Header from "../components/Header";
-import Footer from "../components/Footer";
 
 const HomeScreen: React.FC = () => {
   return (
-    <View>
-      <Header />
-      <Text>Welcome to the Home Screen!</Text>
-      <Text>Enjoy exploring the app!</Text>
-      <Footer />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Header />
+        <Text>Welcome to the Home Screen!</Text>
+        <Text>Enjoy exploring the app!</Text>
+      </View>
+    </SafeAreaView>
   );
 };
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default HomeScreen;
