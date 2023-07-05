1a7463d66c4344df6f059ba84f66c1c2c515042e
Guillermina Lorenzo Martinez <glm1a23@soton.ac.uk>
2023-07-05 14:11:55 +0100

    Updated App and Home

--- App.tsx
+++ App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import AppLayout from "./components/AppLayout";
 import HomeScreen from "./screens/Home";
 
 const Stack = createNativeStackNavigator();
@@ -11,14 +10,9 @@ const App: React.FC = () => {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen
-          name="AppLayout"
-          component={AppLayout}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
           name="Home"
           component={HomeScreen}
-          options={{ title: "Welcome" }}
+          options={{ headerShown: false }}
         />
       </Stack.Navigator>
     </NavigationContainer>
