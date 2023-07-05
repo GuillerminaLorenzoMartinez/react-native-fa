d0f5d1839d4ed4b4efeeba1ca9a4ebb1a1ca0074
Guillermina Lorenzo Martinez <glm1a23@soton.ac.uk>
2023-07-05 16:49:36 +0100

    Updated app, added TabNavigator

--- App.tsx
+++ App.tsx
@@ -1,20 +1,20 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeScreen from "./screens/Home";
 
-const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 const App: React.FC = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
+      <Tab.Navigator>
+        <Tab.Screen
           name="Home"
           component={HomeScreen}
           options={{ headerShown: false }}
         />
-      </Stack.Navigator>
+      </Tab.Navigator>
     </NavigationContainer>
   );
 };
