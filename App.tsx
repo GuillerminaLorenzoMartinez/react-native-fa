8cad439f525e2d8aa1b4cf796b91c5892e844ea5
Guillermina Lorenzo Martinez <glm1a23@soton.ac.uk>
2023-07-06 11:33:06 +0100

    Updated App, added tab navigator

--- App.tsx
+++ App.tsx
@@ -2,16 +2,24 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeScreen from "./screens/Home";
+import AwardsAndOutputsScreen from "./screens/AwardsAndOutputs";
+import Logo from "./components/Logo";
 
 const Tab = createBottomTabNavigator();
 
 const App: React.FC = () => {
   return (
     <NavigationContainer>
+      <Logo />
       <Tab.Navigator>
         <Tab.Screen
           name="Home"
           component={HomeScreen}
+          options={{ headerShown: false }}
+        />
+        <Tab.Screen
+          name="AwardsAndOutputs"
+          component={AwardsAndOutputsScreen}
           options={{ headerShown: false }}
         />
       </Tab.Navigator>
