2dd11daefa44103ed341cbe49f5ef493ce104b8e
Guillermina Lorenzo Martinez <glm1a23@soton.ac.uk>
2023-07-06 11:31:39 +0100

    Updated HomeScreen

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,15 +1,20 @@
 import React from "react";
-import { View, Text, StyleSheet, SafeAreaView } from "react-native";
-import Header from "../components/Header";
+import { View, Text, StyleSheet, SafeAreaView, ScrollView } from "react-native";
+import SearchBar from "../components/SearchBar";
 
 const HomeScreen: React.FC = () => {
+  const handleSearch = () => {
+    console.log("Search clicked");
+  };
   return (
     <SafeAreaView style={styles.container}>
-      <View>
-        <Header />
-        <Text>Welcome to the Home Screen!</Text>
-        <Text>Enjoy exploring the app!</Text>
-      </View>
+      <ScrollView>
+        <View>
+          <SearchBar onSearch={handleSearch} />
+          <Text>Welcome to the Home Screen!</Text>
+          <Text>Enjoy exploring the app!</Text>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
